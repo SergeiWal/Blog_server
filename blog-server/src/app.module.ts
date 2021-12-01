d673b2be6a07438bc9cd77d904cbd0eaeb507352
SergeiWal <korovaabc@gmail.com>
2021-12-01 15:05:24 +0300

    Added config with .env

--- blog-server/src/app.module.ts
+++ blog-server/src/app.module.ts
@@ -5,12 +5,12 @@ import { ArticlesModule } from './articles/articles.module';
 import { CommentsModule } from './comments/comments.module';
 import { UsersModule } from './user/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://SergeiValko:R5cIiEXgfvm7kFGj@bstu.hhzuf.mongodb.net/bstu?retryWrites=true&w=majority',
-    ),
+    ConfigModule.forRoot({ envFilePath: 'app.env' }),
+    MongooseModule.forRoot(process.env.DB_URL),
     CommentsModule,
     UsersModule,
     ArticlesModule,
