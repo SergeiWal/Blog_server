605b23593ad72a3e2e178b9236919a7aed51af10
SergeiWal <korovaabc@gmail.com>
2021-12-21 10:41:47 +0300

    Fixed api description (swagger UI)

--- blog-server/src/auth/auth.controller.ts
+++ blog-server/src/auth/auth.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { Roles } from 'src/roles/role.decotator';
-import { Role } from 'src/roles/role.enum';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
